b23c62880442219a6e2849c36e40e16599450c9b
PratikMate <pratikm470@gmail.com>
2022-11-08 13:33:23 +0530

    Solved: Assignment 11.3 DSA problem

---
+++ Assignment 11.3/Answer.js
@@ -0,0 +1,43 @@
+let height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+// output = 6
+
+let height2 = [4, 2, 0, 3, 2, 5];
+// output = 9
+
+/*
+   Time complexity of this code for all cases will be O(n);
+   Space complexity of this code is O(n);
+*/
+
+const trapWater = (height) => {
+    let right = new Array(height.length);
+
+    let max = 0;
+    for (let i = height.length - 1; i >= 0; i--) {
+        right[i] = max;
+        if (height[i] > max) {
+            max = height[i];
+        }
+    }
+
+    max = 0
+    let ans = 0;
+    for (let i = 0; i < height.length; i++) {
+        //left[i] = max;
+        if (i >= 1) {
+            let x = Math.min(max, right[i])
+            if (height[i] < x) {
+                //console.log(i,Math.min(left[i],right[i]),height[i])
+                ans += x - height[i];
+            }
+        }
+
+        if (height[i] > max) {
+            max = height[i];
+        }
+    }
+    return ans;
+}
+
+console.log(trapWater(height));
+console.log(trapWater(height2));
